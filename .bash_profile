819f15e22a15b9283f4c0ac2976aa03f88566df3
Seth Moore <smoore@forever.com>
2015-09-28 13:15:00 -0400

    Move all dots into dot subdir

--- .bash_profile
+++
@@ -1,7 +0,0 @@
-shopt -s cmdhist
-shopt -s histappend
-shopt -s no_empty_cmd_completion
-
-source ~/.bashrc
-source ~/.aliases
-source ~/.auth
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-echo $0
-for file in $(git ls-tree master  ../ --name-only); do
-  echo $(basename $file)
+#!/bin/sh
+#echo $0
+for file in $(git ls-tree master  ../dot/ --name-only); do
+  file=$(basename $file)
+  #echo $PWD/$file
+  ln -vs "$PWD/$file" $HOME/$file
 done
