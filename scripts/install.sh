8bb969c3b2c5470ec9bb8510eedbcf0902659b5d
Seth Moore <smoore@forever.com>
2015-09-28 13:22:21 -0400

    Fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,8 @@
 #echo $0
 for file in $(git ls-tree master  ../dot/ --name-only); do
   file=$(basename $file)
+  cwd=$(echo $PWD | sed 's_scripts_dot_')
+  #echo $cwd
   #echo $PWD/$file
-  ln -vs "$PWD/$file" $HOME/$file
+  ln -vs "$cwd/$file" $HOME/$file
 done
