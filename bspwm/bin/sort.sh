6b5048d0ab1942bda5ce9904b348dc6429f8c1ac
Seth Moore <sethmoore@protonmail.com>
2018-01-04 10:21:34 -0500

    Add lemonbar sorting script...

--- bspwm/bin/sort.sh
+++ bspwm/bin/sort.sh
@@ -2,7 +2,7 @@
 PANEL_WM_NAME=lemonybar
 
 wid=$(xdo id -a "$PANEL_WM_NAME")
-tries_left=20
+tries_left=100
 while [ -z "$wid" -a "$tries_left" -gt 0 ] ; do
   sleep 0.05
   wid=$(xdo id -a "$PANEL_WM_NAME")
