78f06b702059ad0d3941362cb3cf15e3ad598403
Seth Moore <sethmoore@protonmail.com>
2018-06-21 14:10:56 -0400

    wallpaper: update scripts

--- wallpapers/bin/randopaper.py
+++ wallpapers/bin/randopaper.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 import os
+import sys
 from random import choice as draw_straws
 
-def main():
-    home = os.environ.get("HOME")
-    if not home:
-        os.exit(2)
 
-    wallpaper_dir = os.path.join(home, ".wallpapers")
+def main():
+    if len(sys.argv) < 2:
+        directory = os.environ.get("HOME")
+        if not directory:
+            os.exit(2)
+        wallpaper_dir = os.path.join(directory, ".wallpapers")
+    else:
+        wallpaper_dir = sys.argv[1]
 
     ls = os.listdir(wallpaper_dir)
     result = os.path.join(wallpaper_dir, draw_straws(ls))
