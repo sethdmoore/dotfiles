fcff4e98fb69be02071cd3f76c744e25c2043ec0
Seth Moore <sethmoore@protonmail.com>
2024-09-06 12:09:37 -0400

    Allow easily adjusting VPN MTU for tethering.

---
+++ osx/.local/bin/adjust_mtu.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Some VPNs require a lower MTU when tethering
+# Let's make this process painless
+
+VPN_MTU="1350"
+VPN_DEV_PREFIX="utun"
+ROUTE_CHECK_HOST="8.8.8.8"
+
+get_interface() {
+  route get "$ROUTE_CHECK_HOST" \
+    | grep interface \
+    | awk -F ':' '{print $2}' \
+    | tr -d ' '
+}
+
+check_interface() {
+  interface="$1"
+
+  if ! echo "$interface" | grep -q "$VPN_DEV_PREFIX"; then
+    echo "Want: interface='${VPN_DEV_PREFIX}#'"
+    echo "Got:  interface='${interface}'"
+    echo "Are you connected to the VPN?"
+    exit 1
+  fi
+}
+
+
+set_mtu() {
+  interface="$1"
+
+  sudo ifconfig \
+    "${interface}" \
+    mtu "${VPN_MTU}"
+}
+
+
+main() {
+  if ! interface="$(get_interface)"; then
+    echo "Failed to get primary interface"
+    exit 2
+  fi
+
+  check_interface "$interface"
+
+  if ! set_mtu "$interface"; then
+    echo "Failed to set MTU"
+    exit 2
+  fi
+}
+
+
+main "$@"
