7ac9f3444e1be4459a6e6a279049ec36739b964a
Seth Moore <sethmoore@protonmail.com>
2026-05-09 09:36:11 -0400

    hyprland: split it up

--- hyprland/.local/bin/hypr/toggle_hdr.sh
+++ hyprland/.local/bin/hypr/toggle_hdr.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 
-colormode=$(hyprctl monitors -j \
-       | jq -r '.[].colorManagementPreset')
+monitor='HDMI-A-1'
 
-sdr_bitdepth='keyword monitorv2[HDMI-A-1]:bitdepth 8'
-sdr_colormode='keyword monitorv2[HDMI-A-1]:cm auto'
+colormode=$(
+  hyprctl monitors -j \
+  | jq -r --arg monitor "$monitor" '.[] 
+           | select(.name == $monitor)
+           | .colorManagementPreset
+          '
+)
+
+sdr_bitdepth="keyword monitorv2[${monitor}]:bitdepth 8"
+sdr_colormode="keyword monitorv2[${monitor}]:cm auto"
 # hdr_bitdepth='keyword monitorv2[HDMI-A-1]:bitdepth 10'
 # hdr_colormode='keyword monitorv2[HDMI-A-1]:cm hdr'
 batch_cmd="$sdr_bitdepth; $sdr_colormode"
 
-if [ "$colormode" = "hdr" ]; then
+if [ "$colormode" = "hdr" ] || [ "$colormode" = "hdredid" ]; then
+# if [ "$colormode" = "hdr" ]; then # || [ "$colormode" = "hdredid" ]; then
   hyprctl --batch "$batch_cmd"
   notify-send "HDR disabled"
 elif [ "$colormode" = "srgb" ]; then
   # colormode should be srgb, reload
   hyprctl reload
-  notify-send "HDR enabled"
+  notify-send "HDR" "Enabled"
 else
-  echo "warning: unknown color mode"
-  echo "warning: reloading..."
+  notify-send "HDR" "${colormode} colormode is unsupported\nReloading config to reset"
   hyprctl reload
 fi
