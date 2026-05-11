ccfb6997f0cfbef37d5c59450fdca305a316962d
Seth Moore <sethmoore@protonmail.com>
2026-05-11 14:05:34 -0400

    astronvim: new templates

--- astronvim/.config/nvim/lua/community.lua
+++ astronvim/.config/nvim/lua/community.lua
@@ -1,4 +1,4 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,8 +7,6 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.lua" },
-  -- { import = "astrocommunity.game.leetcode-nvim"}
-  { import = "astrocommunity.recipes.ai" },
+  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
 }
--- astronvim/.config/nvim/lua/polish.lua
+++ astronvim/.config/nvim/lua/polish.lua
@@ -1,32 +1,19 @@
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
+-- 2026-05-09: disable polish, see if we miss it.
+if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- This will run last in the setup process.
+-- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Set up custom filetypes
+-- 2026-05-09: these were disabled
 -- vim.filetype.add {
 --   extension = {
---     foo = "fooscript",
---   },
---   filename = {
---     ["Foofile"] = "fooscript",
---   },
---   pattern = {
---     ["~/%.config/foo/.*"] = "fooscript",
+--     jenkinsfile = "groovy"
 --   },
 -- }
-vim.filetype.add {
-  extension = {
-    jenkinsfile = "groovy"
-  },
-}
--- 2025-01-02 workaround for terraformls not detecting bare terraform files
-vim.filetype.add {
-  extension = {
-    tf = "terraform"
-  }
-}
-
--- require("lspconfig").terraformls.setup {
---     filetypes = { "terraform", },
---     cmd = { 'terraform-ls', 'serve', '-log-file', vim.fs.dirname(require('vim.lsp.log').get_filename()) .. "/terraform-ls.log" },
+-- -- 2025-01-02 workaround for terraformls not detecting bare terraform files
+-- vim.filetype.add {
+--   extension = {
+--     tf = "terraform"
+--   }
 -- }
